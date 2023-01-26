2cf2db0daffc540b44b95dfaeb120278a2801e48
JRSeabaugh1s <JRSeabaugh1s@capetigers.com>
2023-01-26 10:33:49 -0600

    make clock countdown

--- tny_timer.js
+++ tny_timer.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
@@ -10,6 +11,33 @@
 */
 
 
+
+
+
+showClock();
+setInterval("showClock()", 1000);
+
+function showClock() {
+    var thisDay = new Date();
+    var localDate = thisDay.toLocaleDateString();
+    var localTime = thisDay.toLocaleTimeString();
+    
+    document.getElementById("currentTime").innerHTML = localDate + "<br />" + localTime;
+    
+    var j4Date = nextJuly4(thisDay);
+    j4Date.setHours(21);
+    
+    var days = (j4Date - thisDay)/(1000*60*60*24);
+    var hrs = (days - Math.floor(days))*24;
+    var mins = (hrs - Math.floor(hrs))*60;
+    var secs = (mins - Math.floor(mins))*60;
+    
+    document.getElementById("dLeft").textContent = Math.floor(days);
+    document.getElementById("hLeft").textContent = Math.floor(hrs);
+    document.getElementById("mLeft").textContent = Math.floor(mins);
+    document.getElementById("sLeft").textContent = Math.floor(secs);
+}
+
 function nextJuly4(currentDate) {
    var cYear = currentDate.getFullYear();
    var jDate = new Date("July 4, 2018");
@@ -17,14 +45,4 @@ function nextJuly4(currentDate) {
    if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
    return jDate;
 }
-
-function showClock() {
-    var thisDay = new Date("May, 19 2018 9:31:27 a.m.");
-    var localDate = thisDay.toLocaleDateString();
-    var localTime = thisDay.toLocaleTimeString();
-
-    document.getElementById("currentTime").innerHTML = localDate + "<br />" + localTime;
-
-    if (nextJuly4(thisDay));
-    return jDate;
-}
+ 
